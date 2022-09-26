7f6e0e38c4ccd306082b0f226b5b680c2f5a6576
Lynn <lynn1_chen@asus.com>
2022-09-26 10:13:04 +0800

    Fixed - OrbitControls

--- src/components/page-main/page-main.js
+++ src/components/page-main/page-main.js
@@ -1,7 +1,7 @@
 
 import * as THREE from "three";
 
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default {
   name: 'page-main',
