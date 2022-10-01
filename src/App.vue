5b128d5e54f8a551275da893c927ccdc229b32ae
Lynn <lynn1_chen@asus.com>
2022-10-01 22:36:07 +0800

    Added - page about

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,40 @@ body{
   margin: 0;
   padding: 0;
 }
+
+ /**
+ tooltip menu **/
+
+.ant-tooltip.ant-menu-inline-collapsed-tooltip.ant-tooltip-placement-right .ant-tooltip-inner{
+  display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+
+}
+ .ant-tooltip.ant-menu-inline-collapsed-tooltip.ant-tooltip-placement-right .style--active{
+    display: none;
+ }
+  .ant-tooltip.ant-menu-inline-collapsed-tooltip.ant-tooltip-placement-right .style--normal{
+    width: 20px;
+    display:inline-block;
+    margin: 0 auto;
+ }
+ .ant-tooltip.ant-menu-inline-collapsed-tooltip.ant-tooltip-placement-right .icon-block{
+  display: flex;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+ }
+
+ .ant-tooltip.ant-menu-inline-collapsed-tooltip.ant-tooltip-placement-right .menu-txt{
+   font-size: 12px;
+    display: block;
+    width: 100%;
+    justify-content: center;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    margin-top: 4px;
+}
+
 </style>
--- src/components/page-about/page-about.js
+++ src/components/page-about/page-about.js
@@ -1,21 +1,32 @@
 
+import {reactive} from 'vue';
 export default {
   name: 'page-about',
   components: {},
   props: [],
-  data () {
+  setup(){
+    let members = reactive([{
+      memberName: 'Jun-Ting Chen',
+      position: 'Frontend'
+    },{
+      memberName: 'Si-Yu Wei',
+      position: 'Data Processing'
+    },{
+      memberName: 'Wei Hong',
+      position: 'UX Designer'
+    },{
+      memberName: 'Yi-Hsuan Tsai',
+      position: 'Animator'
+    },{
+      memberName: 'Jia-Fang Hsu',
+      position: 'UI Designer'
+    },{
+      memberName: 'Kuan-Ling Chen',
+      position: 'Frontend'
+    }]);
     return {
-
+      members
     }
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-
   }
 }
 
